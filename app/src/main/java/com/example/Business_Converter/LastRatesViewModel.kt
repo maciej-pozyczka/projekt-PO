fd14fa5c97231e5c2a5c4eec1937358c64729ab9
Maciej Pozyczka <maciej.pozyczka@wp.pl>
2021-05-10 19:32:04 +0200

    wykresy każdej waluty

---
+++ app/src/main/java/com/example/Business_Converter/LastRatesViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.Business_Converter
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.Business_Converter.CoursesCalculator.models.SingleRateModel
+import com.example.Business_Converter.network.Repository
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class LastRatesViewModel(private val repository: Repository): ViewModel() {
+
+    val ratesResponse: MutableLiveData<Response<SingleRateModel>> = MutableLiveData()
+
+    fun getSomeRates(code: String, number: Int) {
+        viewModelScope.launch {
+            val response = repository.getSomeRates(code, number)
+            ratesResponse.value = response
+        }
+    }
+}
